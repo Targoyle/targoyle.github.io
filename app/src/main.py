ccaeff3ab12b1623c5348c05923c7e2d346f180f
Targoyle <Targoyle@gmail.com>
2023-10-06 14:31:17 +0900

    test

--- app/src/main.py
+++ app/src/main.py
@@ -1,3 +1,4 @@
+"""
 from fastapi import FastAPI
 
 from routers import default
@@ -5,3 +6,13 @@ from routers import default
 app = FastAPI()
 
 app.include_router(default.router)
+"""
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"Hello": "World"}
