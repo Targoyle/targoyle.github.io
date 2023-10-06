9583cbd9796ad3f34e3130b3af6cab43923ac8a4
Targoyle <Targoyle@gmail.com>
2023-10-06 15:14:09 +0900

    test

--- app/src/main.py
+++ app/src/main.py
@@ -1,13 +1,16 @@
-'''
 from fastapi import FastAPI
 
 from routers import default
 
-app = FastAPI()
+app = FastAPI(
+    openapi_url="/openapi.json",  # "/openapi.json" or None
+    docs_url="/docs",  # "/docs" or None
+    redoc_url="/redoc",  # "/redoc" or None
+)
 
 app.include_router(default.router)
-'''
 
+'''
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -16,3 +19,4 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"Hello": "World src"}
+'''
