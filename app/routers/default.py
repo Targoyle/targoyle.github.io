60f94d73ccf2ff0c62c6da81ded9c716792396f6
Targoyle <Targoyle@gmail.com>
2023-12-06 15:41:42 +0900

    fix

--- app/routers/default.py
+++ app/routers/default.py
@@ -82,12 +82,11 @@ def location(request: Request):
     ip = headers.get("x-forwarded-for") or headers.get("x-vercel-proxied-for")
     latitude = headers.get("x-vercel-ip-latitude")
     longitude = headers.get("x-vercel-ip-longitude")
+    city = headers.get("x-vercel-ip-city")
     country = headers.get("x-vercel-ip-country")
     timezone = headers.get("x-vercel-ip-timezone")
 
-    try:
-        city = headers.get("x-vercel-ip-city")
-    except Exception as e:
+    if not city:
         city = "-"
 
     return {
